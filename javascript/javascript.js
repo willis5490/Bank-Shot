d14f8255fcaec12e678400083673914f3a517d8f
William Stearns <wcstearns@yahoo.com>
2018-08-09 11:54:05 -0600

    k

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,31 +1,31 @@
-var config = {
-    apiKey: "AIzaSyCqfdA_8p8gRVgn0PKMuqGiObrw6sbwrAE",
-    authDomain: "upcoming-shows-a5710.firebaseapp.com",
-    databaseURL: "https://upcoming-shows-a5710.firebaseio.com",
-    projectId: "upcoming-shows-a5710",
-    storageBucket: "upcoming-shows-a5710.appspot.com",
-    messagingSenderId: "978194046550"
-  };
-  firebase.initializeApp(config);
+// var config = {
+//     apiKey: "AIzaSyCqfdA_8p8gRVgn0PKMuqGiObrw6sbwrAE",
+//     authDomain: "upcoming-shows-a5710.firebaseapp.com",
+//     databaseURL: "https://upcoming-shows-a5710.firebaseio.com",
+//     projectId: "upcoming-shows-a5710",
+//     storageBucket: "upcoming-shows-a5710.appspot.com",
+//     messagingSenderId: "978194046550"
+//   };
+//   firebase.initializeApp(config);
 
-  var database = firebase.database();
+//   var database = firebase.database();
   
 
-  var newEmp = {
-    name: empName,
-    role: empRole,
-  };
+//   var newEmp = {
+//     name: empName,
+//     role: empRole,
+//   };
 
   
-  database.ref().push(newEmp);
+//   database.ref().push(newEmp);
 
-  database.ref().on("child_added", function(childSnapshot) {
-    console.log(childSnapshot.val());
+//   database.ref().on("child_added", function(childSnapshot) {
+//     console.log(childSnapshot.val());
   
-    // Store everything into a variable.
-    var empName = childSnapshot.val().name;
-    var empRole = childSnapshot.val().role;
-  })
+//     // Store everything into a variable.
+//     var empName = childSnapshot.val().name;
+//     var empRole = childSnapshot.val().role;
+//   })
 
 
 
